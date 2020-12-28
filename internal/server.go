8733d9146df5f9620c0f47f9d6bff06930d24ea1
Amarjeet Singh Rai <jatt27@gmail.com>
2020-12-28 13:50:07 +0000

    Move logic out of main

--- internal/server.go
+++ internal/server.go
@@ -1,17 +1,58 @@
 package internal
 
 import (
+	"context"
+	"fmt"
+	"go.uber.org/zap"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"webhook-server-test/internal/service"
 )
 
-func NewServer(addr string, svc *service.Service) *http.Server {
+type Server struct {
+	*http.Server
+	l *zap.Logger
+}
+
+func NewServer(addr string, logger *zap.Logger, svc *service.Service) *Server {
 	mux := http.NewServeMux()
 	mux.Handle("/webhooks", http.HandlerFunc(svc.CreateWebhooksHandler))
 	mux.Handle("/fire-webhooks", http.HandlerFunc(svc.FireWebhooksHandler))
 
-	return &http.Server{
-		Addr:    addr,
-		Handler: mux,
+	return &Server{
+		l: logger,
+		Server: &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		},
 	}
+}
+
+func (s *Server) StartWithGracefulShutdown() {
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		s.l.Info("server is shutting down")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+		defer cancel()
+
+		s.Server.SetKeepAlivesEnabled(false)
+		if err := s.Server.Shutdown(ctx); err != nil {
+			s.l.Error(fmt.Sprintf("graceful shutdown failed: %v\n", err))
+		}
+		close(done)
+	}()
+
+	if err := s.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		s.l.Fatal(fmt.Sprintf("server start failed: %v\n", err))
+	}
+
+	<-done
+	s.l.Info("server stopped")
 }
